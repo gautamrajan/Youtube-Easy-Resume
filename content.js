9afc5a0b92a745633ad64675f8c08db9f2165aee
Gautam Rajan <grajan@usc.edu>
2021-03-13 00:02:27 -0800

    time logging working correctly

--- content.js
+++ content.js
@@ -1,3 +1,28 @@
 //content.js
 
-var ytplayer = document.getElementById
+//$.getscript("top/www.youtube.com/s/player/d29f3109/player_ias.vflset/en_US/base.js")
+/* var currentURL = window.location.href;
+var currentTime = g.k.getCurrentTime;
+
+console.log("Current URL: " + currentURL);
+console.log("Current time: " + currentTime);
+ */
+
+/* function runScriptOnPage() {
+    var script = document.createElement('script');
+    script.textContent = "console.log(\"Current Time\" + g.k.getCurrentTime());";
+    (document.head||document.documentElement).appendChild(script);
+    script.remove();
+} */
+var currentURL = window.location.href;
+var video = document.querySelector("video");
+console.log("Current URL: " + currentURL);
+/* function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+} */
+
+var ct = video.currentTime;
+video.ontimeupdate = function(){
+    ct = video.currentTime;
+    console.log("Current time: " + ct);
+};
