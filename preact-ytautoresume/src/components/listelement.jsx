e3b9533b8b79a67ddecaee362d7b1b91662c71cd
gautamrajan <grajan@usc.edu>
2024-10-04 16:38:41 +0530

    Implemented shift+click multiple video selection

--- preact-ytautoresume/src/components/listelement.jsx
+++ preact-ytautoresume/src/components/listelement.jsx
@@ -1,14 +1,15 @@
 import { h } from 'preact';
 import { extractWatchID, secondsToHMS } from './utilities';
 const DEBUG = false;
+
 export default function ListElement(props) {
-    //props -> video, edit, selectedVideos, marginRight, maxBarWidth, eClickHandler
+    //props -> video, index, edit, selectedVideos, marginRight, maxBarWidth, eClickHandler
     let video = props.video;
     let opts = {};
     let selectorName = ""; 
     if (props.edit) {
-        if (props.selectedVideos.some(vid => video.videolink === vid.videolink)) {
-            console.log("VIDEO SELECTED!");
+        if (props.selectedVideos.some(vid => extractWatchID(video.videolink) === extractWatchID(vid.videolink))) {
+            DEBUG && console.log("VIDEO SELECTED!");
             selectorName = selectorName + " selected";
         }
         else {
@@ -16,28 +17,35 @@ export default function ListElement(props) {
         }
     }
     if (!props.edit) { opts["href"] = video.videolink;}
+
+    const handleClick = (event) => {
+        if (props.edit) {
+            props.eClickHandler(video, props.index, event);
+        }
+    };
+
     return (
-        <div className={`list-element-container`} onClick={()=>props.eClickHandler(video)}>
+        <div className={`list-element-container`} onClick={handleClick}>
             <a className={`main-list-element${selectorName}`} {...opts} target="_blank" title={video.title}
                 style={`margin-right: ${props.marginRight}px;`}>
-            <img src={`https://img.youtube.com/vi/${extractWatchID(video.videolink)}/default.jpg`} width="120" height="90"/>
-            <div className={`element-body`}>
-                <info>
-                    <videoTitle>
-                        {video.title}
-                    </videoTitle>
-                    <subtext>
-                        {video.channel}
-                    </subtext>
-                </info>
-                <div className="time-display">
-                    <timeInfo>
-                        {video.time<0 ? <currentTime>0:00</currentTime>:<currentTime>{secondsToHMS(video.time)}</currentTime>}
-                        <duration>{secondsToHMS(video.duration)}</duration>
-                    </timeInfo>
-                    <bar style={`width:${Math.round((video.time/video.duration)*props.maxBarWidth)}px`}></bar>
+                <img src={`https://img.youtube.com/vi/${extractWatchID(video.videolink)}/default.jpg`} width="120" height="90"/>
+                <div className={`element-body`}>
+                    <info>
+                        <videoTitle>
+                            {video.title}
+                        </videoTitle>
+                        <subtext>
+                            {video.channel}
+                        </subtext>
+                    </info>
+                    <div className="time-display">
+                        <timeInfo>
+                            {video.time<0 ? <currentTime>0:00</currentTime>:<currentTime>{secondsToHMS(video.time)}</currentTime>}
+                            <duration>{secondsToHMS(video.duration)}</duration>
+                        </timeInfo>
+                        <bar style={`width:${Math.round((video.time/video.duration)*props.maxBarWidth)}px`}></bar>
+                    </div>
                 </div>
-            </div>
             </a>
             <style jsx>{`   
                 .unselected {   
@@ -49,9 +57,26 @@ export default function ListElement(props) {
                 timeInfo {
                     cursor: pointer;
                 }
-
+                    .list-element-container {
+                    user-select: none;
+                    -webkit-user-select: none;
+                    -moz-user-select: none;
+                    -ms-user-select: none;
+                }
+                .unselected {   
+                    opacity: 0.4;
+                }
+                info {
+                    cursor: pointer;
+                }
+                timeInfo {
+                    cursor: pointer;
+                }
+                img, videoTitle, subtext, currentTime, duration {
+                    pointer-events: none;
+                }
             `}
             </style>
-    </div>  
+        </div>  
     )
 }
